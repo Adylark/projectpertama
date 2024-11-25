89e34f82e427edfbfc45ec4bcce77dde4aef87f6
Adylark <fadhilah0902@gmail.com>
2024-11-25 10:09:45 +0700

    projectpertama

---
+++ Tm13/src/Viewer/ReportFix.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Viewer;
+
+/**
+ *
+ * @author Fadhilah Khusnul K
+ */
+
+public class ReportFix {
+    public static void main(String [] args){
+        new InputDosen().setVisible(true);
+    }
+}
